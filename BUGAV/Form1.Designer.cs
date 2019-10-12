35a8c3a7ba8741cd46ad533d1c499bc7c601fe40
bpopov <borisiswork@gmail.com>
2019-10-12 19:28:18 +0300

    empty

---
+++ BUGAV/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace BUGAV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.controlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.staticAnalyzisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.realtimeProtectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cloudScanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.networkProtectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.controlToolStripMenuItem,
+            this.filterToolStripMenuItem,
+            this.staticAnalyzisToolStripMenuItem,
+            this.realtimeProtectionToolStripMenuItem,
+            this.cloudScanToolStripMenuItem,
+            this.networkProtectionToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1597, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // controlToolStripMenuItem
+            // 
+            this.controlToolStripMenuItem.Name = "controlToolStripMenuItem";
+            this.controlToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.controlToolStripMenuItem.Text = "control";
+            // 
+            // filterToolStripMenuItem
+            // 
+            this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.filterToolStripMenuItem.Text = "filter";
+            // 
+            // staticAnalyzisToolStripMenuItem
+            // 
+            this.staticAnalyzisToolStripMenuItem.Name = "staticAnalyzisToolStripMenuItem";
+            this.staticAnalyzisToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.staticAnalyzisToolStripMenuItem.Text = "static analysys";
+            // 
+            // realtimeProtectionToolStripMenuItem
+            // 
+            this.realtimeProtectionToolStripMenuItem.Name = "realtimeProtectionToolStripMenuItem";
+            this.realtimeProtectionToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.realtimeProtectionToolStripMenuItem.Text = "realtime protection";
+            // 
+            // cloudScanToolStripMenuItem
+            // 
+            this.cloudScanToolStripMenuItem.Name = "cloudScanToolStripMenuItem";
+            this.cloudScanToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.cloudScanToolStripMenuItem.Text = "cloud scan";
+            // 
+            // networkProtectionToolStripMenuItem
+            // 
+            this.networkProtectionToolStripMenuItem.Name = "networkProtectionToolStripMenuItem";
+            this.networkProtectionToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.networkProtectionToolStripMenuItem.Text = "network protection";
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.updateToolStripMenuItem.Text = "update";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1597, 736);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem controlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem staticAnalyzisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem realtimeProtectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cloudScanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem networkProtectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+    }
+}
+
