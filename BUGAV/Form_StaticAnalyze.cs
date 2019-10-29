9562204f0a0b12cd845616f81bea0da32264081c
bpopov <borisiswork@gmail.com>
2019-10-29 19:33:01 +0300

    new forms + libs

---
+++ BUGAV/Form_StaticAnalyze.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarkUI.Forms;
+
+namespace BUGAV {
+    public partial class Form_StaticAnalyze : DarkForm {
+        public Form_StaticAnalyze() {
+            InitializeComponent();
+        }
+    }
+}
